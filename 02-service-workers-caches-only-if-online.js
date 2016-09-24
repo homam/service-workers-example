0fabb4db7fb0e0e420dff5f321e3e1a871b748f4
Homam Hosseini <bluesnowball@gmail.com>
2016-09-24 16:11:44 +0200

    Update 02-service-workers-caches-only-if-online.js

--- 02-service-workers-caches-only-if-online.js
+++ 02-service-workers-caches-only-if-online.js
@@ -2,7 +2,7 @@
 // its response, cacheName is not really used in this service worker.
 // Changing the cacheName will cause other (previous) caches to be deleted
 // (check activate event handler).
-const cacheName = '1'
+const cacheName = 'v1'
 
 // utlity 
 const trace = (x, y) => {
@@ -11,8 +11,16 @@ const trace = (x, y) => {
 }
 
 
+const urlsToCache = new Set([
+  self.location.href
+].map(u => new URL(u, self.location.href).href))
+
+
 self.addEventListener('install', event => {
   console.log('% install')
+  caches.open(cacheName).then(cache =>
+    cache.addAll(Array.from(urlsToCache))
+  )
   event.waitUntil(self.skipWaiting());
 });
 
@@ -21,10 +29,12 @@ self.addEventListener('activate', event => {
 
   // delete "old" cacheName s
   event.waitUntil(
-    caches.keys().then(keys => 
-      Promise.all(
-        keys.filter(key => cacheName != key).map(key =>
-          trace(`# deleting ${key}`, caches.delete(key))
+    self.clients.claim().then(_ =>
+      caches.keys().then(keys => 
+        Promise.all(
+          keys.filter(key => cacheName != key).map(key =>
+            trace(`# deleting ${key}`, caches.delete(key))
+          )
         )
       )
     )
@@ -54,16 +64,25 @@ const tryServingFromCache = request =>
       } else {
         console.log('! not in cache', request.url)
         return fetch(request).then(response => 
-          cacheAResponse(cache, event, response, '$ cached ')
+          cacheAResponse(cache, request, response, '$ cached ')
         )
       }
     })
   );
 
 
-self.addEventListener('fetch', event => 
-  event.respondWith(navigator.onLine
-    ? cacheARequest(event.request)
-    : tryServingFromCache(event.request)
-  )
-);
+self.addEventListener('fetch', event => {
+  if(event.request.method != 'GET') {
+    event.respondWith(fetch(event.request))
+  } else {
+    const url = new URL(event.request.url, self.location.href);
+    if(['.png', '.jpg'].some(x => url.pathname.endsWith(x))) {
+      event.respondWith(tryServingFromCache(event.request));
+    } else {
+      event.respondWith(navigator.onLine
+        ? cacheARequest(event.request)
+        : tryServingFromCache(event.request)
+      )
+    }
+  }
+});
